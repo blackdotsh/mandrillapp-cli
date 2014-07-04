61dcd82181eaed7fed0c0abe92c5001ff5d77b58
blackdotsh <blackice.sh@gmail.com>
2014-07-04 10:01:06 -0500

    code clean up

--- sendmail.sh
+++ sendmail.sh
@@ -8,11 +8,9 @@ from_email="" #who is sending the email
 reply_to="$from_email" #reply email address
 from_name="curl sender" #from name
 
-msg='{ "async": false, "key": "'$key'", "message": { "from_email": "'$from_email'", "from_name": "'$from_name'", "headers": { "Reply-To": "'$reply_to'" }, "return_path_domain": null, "subject": "'$2'", "text": "'$3'", "to": [ { "email": "'$1'", "type": "to" } ] } }'
 
-if [ $# -eq 3 ];
-then
-
+if [ $# -eq 3 ]; then
+	msg='{ "async": false, "key": "'$key'", "message": { "from_email": "'$from_email'", "from_name": "'$from_name'", "headers": { "Reply-To": "'$reply_to'" }, "return_path_domain": null, "subject": "'$2'", "text": "'$3'", "to": [ { "email": "'$1'", "type": "to" } ] } }'
 	results=$(curl -A 'Mandrill-Curl/1.0' -d "$msg" 'https://mandrillapp.com/api/1.0/messages/send.json' -s 2>&1);
 	echo "$results" | grep "sent" -q;
 	if [ $? -ne 0 ]; then
